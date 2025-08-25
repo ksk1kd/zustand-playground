73d1947330d782bc5e8f57350162c948bb78748a
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-08-26 00:37:37 +0900

    tutorial: change tsx to jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Board } from './components/Board'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Board />
+      </div>
+    </>
+  )
+}
+
+export default App
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div className="container">Contents</div>
-    </>
-  )
-}
-
-export default App
